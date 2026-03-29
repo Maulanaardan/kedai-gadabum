8bac8d353132165dde444f026d51083fe87d6d95
maulana <maulanawardana01@gmail.com>
2026-03-29 22:49:38 +0700

    feat: cart and total price

--- frontend/src/app/order/page.tsx
+++ frontend/src/app/order/page.tsx
@@ -10,16 +10,29 @@ type Menu = {
 };
 
 export default function OrderPage() {
+  const [menus, setMenus] = useState<Menu[]>([]);
+  const [cart, setCart] = useState<Menu[]>([]);
+
   const searchParams = useSearchParams();
   const table = searchParams.get("table");
-
-  const [menus, setMenus] = useState<Menu[]>([]);
+  const total = cart.reduce((sum, item) => sum + item.price, 0);
 
   useEffect(() => {
-    fetch("http://localhost:5000/menus")
-      .then((res) => res.json())
-      .then((data) => setMenus(data));
-  }, []);
+  fetch("http://localhost:5000/menus")
+    .then((res) => {
+      console.log("RES:", res);
+      return res.json();
+    })
+    .then((data) => {
+      console.log("DATA:", data);
+      setMenus(data);
+    })
+    .catch((err) => console.error("ERROR:", err));
+}, []);
+
+    const addToCart = (menu: Menu) => {
+    setCart([...cart, menu]);
+  };
 
   return (
     <div>
@@ -30,8 +43,19 @@ export default function OrderPage() {
       {menus.map((menu) => (
         <div key={menu.id}>
           {menu.name} - Rp {menu.price}
+          <button onClick={() => addToCart(menu)}>
+            Tambah
+          </button>
         </div>
       ))}
+
+      <h2>Keranjang:</h2>
+      {cart.map((item, index) => (
+        <div key={index}>
+          {item.name} - Rp {item.price}
+        </div>
+      ))}
+      <h3>Total: Rp {total}</h3>
     </div>
   );
 }
